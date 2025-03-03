fb91ebeab70fc486f93259947db0efe766569fc4
Chirag Jogi <chiragjogi97@gmail.com>
2025-03-03 16:21:20 +0530

    app.py port updated

--- app.py
+++ app.py
@@ -77,6 +77,6 @@ async def predict_route(request:Request,file:UploadFile=File(...)):
 
 
 if __name__ == "__main__":
-    import uvicorn
-    app_run(app, host="0.0.0.0",port=10000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="debug")
    
